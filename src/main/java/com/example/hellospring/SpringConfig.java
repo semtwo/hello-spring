acdc1630d222c64b96b9238c9c589d5b27e6fda9
rnongsiri <comeosm@gmail.com>
2023-02-24 14:43:52 +0900

    service, repository, controller, spring bean 직접 등록, testcode 작성

---
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.example.hellospring;
+
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import com.example.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ service, repository 어노테이션 사용하는 대신 직접 config 설정해서 spring bean 설정하는 방법
+ @Service, @Repository 지우고 써야함
+ @Controller 는 직접 등록하지말고 어노테이션 그대로 사용
+*/
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+    @Bean // 얘는 주석 처리해도 돌아감 왜지?
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
